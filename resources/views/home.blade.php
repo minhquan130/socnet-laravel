5fc9b4819b13a577ce1f1e74893e3501e9960b18
NguyenThu711 <nguyenthu7112003@gmail.com>
2024-10-21 12:09:47 +0700

    Build header in page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+    <script src="{{ asset('js/all.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 </head>
 <body>
-    
+    @include('layouts.header')
 </body>
 </html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,37 @@
+<!-- Header -->
+<header>
+    <div class="header_container">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="{{ asset('images/logo.png') }}" alt="" height="54">
+        </div>
+        <div class="search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" placeholder="Tìm kiếm...">
+        </div>
+        <!-- Mavigation -->
+        <div class="navigation">
+            <div class="home active">
+                <span><i class="fa-solid fa-house"></i></span>
+            </div>
+            <div class="friends">
+                <span><i class="fa-solid fa-user-group"></i></span>
+            </div>
+            <div class="messages">
+                <span><i class="fa-solid fa-comments"></i></span>
+            </div>
+            <div class="messages">
+                <span><i class="fa-solid fa-camera-retro"></i></span>
+            </div>
+        </div>
+        <!-- Header Left -->
+        <div class="header_left">
+            <div class="notification">
+                <i class="fa-solid fa-bell icon-notification"></i>
+            </div>
+            <div class="setting">
+                <i class="fa-solid fa-circle-chevron-down icon-dropdonw"></i>
+            </div>
+        </div>
+    </div>
+</header>
