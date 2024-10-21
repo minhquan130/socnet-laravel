599f7906d3cfabe73892b054dc1b4a7ac9fc0234
machtu678 <machtu678@gmail.com>
2024-10-21 11:47:27 +0700

    Build template page forgetpassword

--- resources/views/forgetpassword.blade.php
+++ resources/views/forgetpassword.blade.php
@@ -5,8 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quên mật khẩu</title>
+    <link rel="stylesheet" href="{{ asset('css/forgetpassword.css') }}">
 </head>
 <body>
-    Quên mật khẩu
+    <div class="forgetPassword-contaiter">
+        <h2>Quên mật Khẩu</h2>
+        <form action="#" method="post" class="form-forgetpassword">
+            <label for>
+                <i class="fa-solid fa-envelope"></i>
+                <input type="text" name="email" id="email"
+                    placeholder="Nhập Email hoặc SĐT đã đăng ký" required>
+            </label>
+           
+            <label for>
+                <i class="fa-solid fa-user"></i>
+                <input type="text" name="Otp" id="name" placeholder="Nhập mã OTP"
+                    required>
+            </label>
+        
+            <label for>
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" name="Changepassword" id="password"
+                    placeholder="Mật khẩu mới" required>
+            </label>
+            
+            <label for>
+                <i class="fa-solid fa-rotate-right"></i>
+                <input type="password" name="agin_password"
+                    id="again_password" placeholder="Xác thực mật khẩu" required>
+            </label>
+          
+            <label for="" class="btn-changepass">
+                 <input type="submit" value="Thay Đổi">
+            </label>
+        </form>
+    </div>
 </body>
 </html>
