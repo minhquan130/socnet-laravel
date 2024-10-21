cc4bce6dd450fd17875ecfb3c98d24211f06101e
dinhvandat03 <dinhvandat.06102003@gmail.com>
2024-10-21 11:34:00 +0700

    Build template page Register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [
+    LoginController::class,
+    'index'
+])->name('login');
+
+Route::get('/register', [
+    RegisterController::class,
+    'index'
+])->name('register');
