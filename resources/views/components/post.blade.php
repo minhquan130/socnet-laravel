401dd86477b22f720966fceab25d7099863b391e
minhquan130 <minhq130@gmail.com>
2024-10-21 13:00:20 +0700

    fix css and src of img tag

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,51 @@
+<div class="container_posts">
+    <div class="posts">
+        <div class="post-bar">
+            <div class="top-post-bar">
+                <div class="img-avatar">
+                    <img src="{{ asset('images/avatar.png') }}" alt="">
+                </div>
+                <div class="form-text-post">
+                    <input type="text" name="" id="" placeholder="Bạn đang nghĩ gì thế?">
+                </div>
+            </div>
+            <hr style="margin: 1rem 0;">
+            <div class="option-post-bar">
+                <div class="option-item">
+                    <i class="fa-regular fa-image"></i>
+                    <span>Ảnh</span>
+                </div>
+                <div class="option-item">
+                    <i class="fa-regular fa-circle-play"></i>
+                    <span>Video</span>
+                </div>
+                <div class="option-item">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span>Địa điểm</span>
+                </div>
+                <div class="option-item">
+                    <span>Đăng</span>
+                </div>
+
+            </div>
+        </div>
+        <div class="list-posts">
+            <div class="post-item">
+                <div class="top-post">
+                    <div class="top-post-tp1">
+                        <div class="avarta-post">
+                            <img src="{{ asset('images/avatar.png') }}" alt="">
+                        </div>
+                        <div class="content-top-post">
+                            <span class="name">Tikay</span>
+                            <span class="posted-time">5 min ago</span>
+                        </div>
+                    </div>
+                    <div class="top-post-tp2">
+                        <div class="other-post"><i class="fa-solid fa-ellipsis"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
