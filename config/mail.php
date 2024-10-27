b7fceb058fa9e7c35f8dc7ce69f570024e1f101a
minhquan130 <minhq130@gmail.com>
2024-10-27 11:48:50 +0700

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -60,10 +60,9 @@ return [
         ],
 
     ],
-    
+
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-
 ];
