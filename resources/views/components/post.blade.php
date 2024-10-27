ddb1d6d124d040923628812794cce15776732994
dinhvandat03 <117971572+dinhvandat03@users.noreply.github.com>
2024-10-27 11:19:36 +0700

    update-post

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,4 +1,44 @@
 <div class="container_posts">
+
+    <!--  làm phàm modal hiển thie bosstrap-->
+
+    <!-- Modal -->
+    <div class="modal fade" id="commentModal" tabindex="-1" aria-labelledby="commentModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="commentModalLabel">Bình luận</h5>
+                    <button type="button" class="btn-close" da ta-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h2 class="fs-5">Musa</h2>
+                    <p>
+                    <img src="{{ asset('images/musa.jpg') }}" alt="Avatar" class="img-fluid rounded-circle me-2"
+                    style="width: 40px; height: 40px;"> 
+                    <span>bà sáu xinh quá nhen</span>
+                    </p>
+                    <hr>
+                    <h2 class="fs-5">Shizuka</h2>
+                    <p>
+                    <img src="{{ asset('images/Shizuka.png') }}" alt="Avatar" class="img-fluid rounded-circle me-2"
+                    style="width: 40px; height: 40px;"> 
+                    <span>Cái môi bả sáu dễ sợ</span>
+                    </p>
+                </div>
+                <form action="" method="get">
+                    <img src="{{ asset('images/avatar.png') }}" alt="Avatar" class="img-fluid rounded-circle me-2"
+                        style="width: 40px; height: 40px;">
+                    <input type="text" name="comment" id="" placeholder=" Bình luận dưới tên Văn Đat"
+                        style="width: 100%; height:20px;">
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--  -->
+
     <div class="posts">
         <form action="{{ route('post.add') }}" method="post" class="post-bar" enctype="multipart/form-data">
             @csrf
@@ -25,7 +65,7 @@
                     <i class="fa-solid fa-location-dot"></i>
                     <span>Địa điểm</span>
                 </div>
-                <button type="submit" class="option-item" >
+                <button type="submit" class="option-item">
                     <span>Đăng</span>
                 </button>
             </div>
@@ -40,137 +80,138 @@
                 <div class="top-post">
                     <div class="top-post-tp1">
                         <div class="avarta-post">
-                            <img src="{{ asset('images/avatar.png') }}" alt="">
-                        </div>
-                        <div class="content-top-post">
-                            <span class="name">Tikay</span>
-                            <span class="posted-time">5 min ago</span>
-                        </div>
-                    </div>
-                    <div class="top-post-tp2">
-                        <div class="other-post"><i class="fa-solid fa-ellipsis"></i></div>
-                    </div>
+                <img src="{{ asset('images/avatar.png') }}" alt="">
+    </div>
+    <div class="content-top-post">
+        <span class="name">Tikay</span>
+        <span class="posted-time">5 min ago</span>
+    </div>
+</div>
+<div class="top-post-tp2">
+    <div class="other-post"><i class="fa-solid fa-ellipsis"></i></div>
+</div>
+</div>
+
+<div class="content-post">
+    <div class="content-text">
+        Thật là vui vẻ
+    </div>
+    <div class="content-media">
+        <img src="{{asset('images/logo.png')}}" alt="">
+    </div>
+</div>
+
+<div class="metrics-post">
+    <div class="count-likes">
+        <span class="icon-count-likes">
+            <i class="fa-solid fa-heart"></i>
+        </span>
+        <span>56</span>
+    </div>
+    <div class="count-comments">
+        <span>67 bình luận</span>
+    </div>
+</div>
+
+<hr>
+
+<div class="options-post">
+    <div class="option option-like">
+        <span class="option-icon icon-like">
+            <i class="fa-solid fa-heart"></i>
+        </span>
+        <span>Thích</span>
+    </div>
+
+    <div class="option option-comment">
+        <span class="option-icon icon-comment">
+            <i class="fa-solid fa-message"></i>
+        </span>
+        <span>Bình luận</span>
+    </div>
+    <div class="option option-coppy">
+        <span class="option-icon icon-coppy">
+            <i class="fa-solid fa-link"></i>
+        </span>
+        <span>Sao chép</span>
+    </div>
+    <div class="option option-share">
+        <span class="option-icon icon-share">
+            <i class="fa-solid fa-share"></i>
+        </span>
+        <span>Sao chép</span>
+    </div>
+</div>
+</div>
+</div> --}}
+
+<div class="list-posts">
+    @foreach($posts as $post)
+    <div class="post-item">
+        <div class="top-post">
+            <div class="top-post-tp1">
+                <div class="avarta-post">
+                    <img src="{{ asset('images/avatar.png') }}" alt="">
                 </div>
-
-                <div class="content-post">
-                    <div class="content-text">
-                        Thật là vui vẻ
-                    </div>
-                    <div class="content-media">
-                        <img src="{{asset('images/logo.png')}}" alt="">
-                    </div>
-                </div>
-
-                <div class="metrics-post">
-                    <div class="count-likes">
-                        <span class="icon-count-likes">
-                            <i class="fa-solid fa-heart"></i>
-                        </span>
-                        <span>56</span>
-                    </div>
-                    <div class="count-comments">
-                        <span>67 bình luận</span>
-                    </div>
-                </div>
-
-                <hr>
-
-                <div class="options-post">
-                    <div class="option option-like">
-                        <span class="option-icon icon-like">
-                            <i class="fa-solid fa-heart"></i>
-                        </span>
-                        <span>Thích</span>
-                    </div>
-                    <div class="option option-comment">
-                        <span class="option-icon icon-comment">
-                            <i class="fa-solid fa-message"></i>
-                        </span>
-                        <span>Bình luận</span>
-                    </div>
-                    <div class="option option-coppy">
-                        <span class="option-icon icon-coppy">
-                            <i class="fa-solid fa-link"></i>
-                        </span>
-                        <span>Sao chép</span>
-                    </div>
-                    <div class="option option-share">
-                        <span class="option-icon icon-share">
-                            <i class="fa-solid fa-share"></i>
-                        </span>
-                        <span>Sao chép</span>
-                    </div>
+                <div class="content-top-post">
+                    <span class="name">Tikay</span>
+                    <span class="posted-time">{{ $post->created_at->diffForHumans() }}</span>
                 </div>
             </div>
-        </div> --}}
-
-        <div class="list-posts">
-            @foreach($posts as $post)
-            <div class="post-item">
-                    <div class="top-post">
-                        <div class="top-post-tp1">
-                            <div class="avarta-post">
-                                <img src="{{ asset('images/avatar.png') }}" alt="">
-                            </div>
-                            <div class="content-top-post">
-                                <span class="name">Tikay</span>
-                                <span class="posted-time">{{ $post->created_at->diffForHumans() }}</span>
-                            </div>
-                        </div>
-                        <div class="top-post-tp2">
-                            <div class="other-post"><i class="fa-solid fa-ellipsis"></i></div>
-                        </div>
-                    </div>
-                    <div class="content-post">
-                        <div class="content-text">
-                            {{ $post->content }}
-                        </div>
-                        @if($post->media_url)
-                            <div class="content-media">
-                                <img src="{{ $post->media_url }}" alt="Hình ảnh bài viết">
-                            </div>
-                        @endif
-                    </div>
-                    <div class="metrics-post">
-                        <div class="count-likes">
-                            <span class="icon-count-likes">
-                                <i class="fa-solid fa-heart"></i>
-                            </span>
-                            <span>56</span>
-                        </div>
-                        <div class="count-comments">
-                            <span>67 bình luận</span>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="options-post">
-                        <div class="option option-like">
-                            <span class="option-icon icon-like">
-                                <i class="fa-solid fa-heart"></i>
-                            </span>
-                            <span>Thích</span>
-                        </div>
-                        <div class="option option-comment">
-                            <span class="option-icon icon-comment">
-                                <i class="fa-solid fa-message"></i>
-                            </span>
-                            <span>Bình luận</span>
-                        </div>
-                        <div class="option option-coppy">
-                            <span class="option-icon icon-coppy">
-                                <i class="fa-solid fa-link"></i>
-                            </span>
-                            <span>Sao chép</span>
-                        </div>
-                        <div class="option option-share">
-                            <span class="option-icon icon-share">
-                                <i class="fa-solid fa-share"></i>
-                            </span>
-                            <span>Chia sẻ</span>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>        
+            <div class="top-post-tp2">
+                <div class="other-post"><i class="fa-solid fa-ellipsis"></i></div>
+            </div>
+        </div>
+        <div class="content-post">
+            <div class="content-text">
+                {{ $post->content }}
+            </div>
+            @if($post->media_url)
+            <div class="content-media">
+                <img src="{{ $post->media_url }}" alt="Hình ảnh bài viết">
+            </div>
+            @endif
+        </div>
+        <div class="metrics-post">
+            <div class="count-likes">
+                <span class="icon-count-likes">
+                    <i class="fa-solid fa-heart"></i>
+                </span>
+                <span>56</span>
+            </div>
+            <div class="count-comments">
+                <span>67 bình luận</span>
+            </div>
+        </div>
+        <hr>
+        <div class="options-post">
+            <div class="option option-like">
+                <span class="option-icon icon-like">
+                    <i class="fa-solid fa-heart"></i>
+                </span>
+                <span>Thích</span>
+            </div>
+            <div class="option option-comment" data-bs-toggle="modal" data-bs-target="#commentModal">
+                <span class="option-icon icon-comment">
+                    <i class="fa-solid fa-message"></i>
+                </span>
+                <span>Bình luận</span>
+            </div>
+            <div class="option option-coppy">
+                <span class="option-icon icon-coppy">
+                    <i class="fa-solid fa-link"></i>
+                </span>
+                <span>Sao chép</span>
+            </div>
+            <div class="option option-share">
+                <span class="option-icon icon-share">
+                    <i class="fa-solid fa-share"></i>
+                </span>
+                <span>Chia sẻ</span>
+            </div>
+        </div>
     </div>
+    @endforeach
+</div>
+</div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Instacon</title>
+    <!-- FontAwesome CSS -->
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-    <script src="{{ asset('js/all.min.js') }}"></script>
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- jQuery (nếu cần cho phần khác của trang) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap Bundle (bao gồm Popper.js cho modal) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- FontAwesome JS (nếu cần) -->
+    <script src="{{ asset('js/all.min.js') }}"></script>
+
 </head>
+
 <body>
     @include('layouts.header')
     @include('layouts.main')
 
     <script src="{{ asset('js/home.js') }}"></script>
 </body>
+
 </html>
