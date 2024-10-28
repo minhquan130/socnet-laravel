e98375e179e343dd0be6b1383a878fb375a10e3a
minhquan130 <minhq130@gmail.com>
2024-10-28 12:07:27 +0700

    t

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,14 @@
     @include('components.sidebar')
     @include('components.post')
     <div class="container_navbar">
-        <div class="navbar"></div>
+        <div class="navbar-content">
+            <h3 class="title-navbar">Bạn bè</h3>
+            <hr>
+            <div class="list-friends">
+                <div class="item-friend">
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </main>
