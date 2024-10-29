48aeb5c91e72b5770dc3858d716db58192b18860
dinhvandat03 <117971572+dinhvandat03@users.noreply.github.com>
2024-10-29 08:28:58 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::get('/delete-post/{id}', [PostController::class, 'deletePost'])->name('post.delete');
 
 // Route cho việc bình luận
-Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('post.store');
 
 Route::get('/post/{id}', [CommentController::class, 'show'])->name('post.show');
 
