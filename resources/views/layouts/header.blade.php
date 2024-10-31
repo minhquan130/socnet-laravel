aced70e7e983e250cc45524b508e98d54c0f70ab
dinhvandat03 <117971572+dinhvandat03@users.noreply.github.com>
2024-10-31 10:53:20 +0700

    update-layout-chat

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,8 +17,9 @@
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
     <script src="{{ asset('js/all.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/friend.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/friend.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/chat.css')}}">
 </head>
 
 <body>
