dea3365be11c64951f04b4b826ffa2b7a03a8f2d
minhquan130 <minhq130@gmail.com>
2024-11-17 10:00:10 +0700

    update mvc

---
+++ resources/views/emails/otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP Code</title>
+</head>
+<body>
+    <h2>Hello,</h2>
+    <p>Your OTP code is: <strong>{{ $otpCode }}</strong></p>
+    <p>Please use this code to complete your authentication process.</p>
+    <p>This code will expire in 5 minutes.</p>
+</body>
+</html>
