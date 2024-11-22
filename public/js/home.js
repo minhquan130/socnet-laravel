e4a8d34e9e8b0758974002e3f840a21f92b1587b
minhquan130 <minhq130@gmail.com>
2024-11-22 13:48:30 +0700

    fix bug

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,5 @@
-const showMediaPost = document.getElementById('show-media-post');
+
 const imgShowMediaPost = document.getElementById('img-show-media-post');
-const inputPicture = document.getElementById('input-picture');
 const arrOtherPost = document.querySelectorAll('.other-post');
 const optionLikes = document.querySelectorAll('.option-like');
 
@@ -10,33 +9,6 @@ arrOtherPost.forEach(otherPost => {
     const optionsItemPost = otherPost.parentElement.querySelector('.options-item-post');
     otherPost.addEventListener('click', () => showOptionItemPost(optionsItemPost));
 })
-
-function chooseFile() {
-    if (inputPicture.files && inputPicture.files[0]) {
-        const file = inputPicture.files[0];
-
-        // Kiểm tra xem tệp có phải là hình ảnh không
-        if (file.type.startsWith('image/')) {
-            showMediaPost.style.display = 'flex';
-            const reader = new FileReader();
-
-            // Khi tệp được đọc xong, hiển thị ảnh
-            reader.onload = function (e) {
-                imgShowMediaPost.src = e.target.result;
-            }
-
-            reader.readAsDataURL(file); // Đọc tệp dưới dạng URL để hiển thị
-        } else {
-            alert('Vui lòng chọn một tệp hình ảnh.');
-        }
-    }
-}
-
-function closeFile() {
-    showMediaPost.style.display = 'none';  // Ẩn phần tử chứa ảnh
-    imgShowMediaPost.src = '';             // Xóa URL của ảnh
-    inputPicture.value = '';               // Reset input file để người dùng có thể chọn tệp mới
-}
 
 function showOptionItemPost(optionsItemPost) {
     // Toggle hiển thị
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,38 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         const defaultAvatar = "{{ asset('images/none-avatar.jpg') }}"; // Tạo biến chứa đường dẫn đến ảnh mặc định
-const btnClosePreview = showMediaPost.querySelector('.close') ?? '';
-inputPicture.addEventListener('change', chooseFile);
-btnClosePreview.addEventListener('click', closeFile);
+        const showMediaPost = document.getElementById('show-media-post');
+        const inputPicture = document.getElementById('input-picture');
+        const btnClosePreview = showMediaPost.querySelector('.close');
+        inputPicture.addEventListener('change', chooseFile);
+        btnClosePreview.addEventListener('click', closeFile);
 
+        function chooseFile() {
+            if (inputPicture.files && inputPicture.files[0]) {
+                const file = inputPicture.files[0];
+
+                // Kiểm tra xem tệp có phải là hình ảnh không
+                if (file.type.startsWith('image/')) {
+                    showMediaPost.style.display = 'flex';
+                    const reader = new FileReader();
+
+                    // Khi tệp được đọc xong, hiển thị ảnh
+                    reader.onload = function(e) {
+                        imgShowMediaPost.src = e.target.result;
+                    }
+
+                    reader.readAsDataURL(file); // Đọc tệp dưới dạng URL để hiển thị
+                } else {
+                    alert('Vui lòng chọn một tệp hình ảnh.');
+                }
+            }
+        }
+
+        function closeFile() {
+            showMediaPost.style.display = 'none'; // Ẩn phần tử chứa ảnh
+            imgShowMediaPost.src = ''; // Xóa URL của ảnh
+            inputPicture.value = ''; // Reset input file để người dùng có thể chọn tệp mới
+        }
     </script>
     <script src="{{ asset('js/home.js') }}"></script>
     </body>
